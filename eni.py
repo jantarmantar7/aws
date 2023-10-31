34afc1807484bdd3e25e0292d90f0f71e117bb50
jantarmantar7 <75736713+jantarmantar7@users.noreply.github.com>
2023-10-31 10:00:06 -0400

    Update eni.py

--- eni.py
+++ eni.py
@@ -3,23 +3,22 @@ from datetime import datetime, timedelta
 
 def lambda_handler(event, context):
     ec2_client = boto3.client('ec2')
-    
-    # Get current time
+
     current_time = datetime.now()
-    
-    # Set the threshold for 30 days
     threshold_time = current_time - timedelta(days=30)
-    
-    # Describe ENIs
+
     response = ec2_client.describe_network_interfaces()
-    
-    # Filter ENIs based on criteria
+
     filtered_enis = [eni for eni in response['NetworkInterfaces'] if
                      not eni.get('Attachment') and
                      eni.get('Status') == 'available' and
                      eni.get('Attachment') is None and
                      eni.get('CreateTime') <= threshold_time]
 
-    # Print or process the filtered ENIs
-    for eni in filtered_enis:
-        print(f"Unused ENI found: {eni['NetworkInterfaceId']}")
+    # Do something with the filtered ENIs (e.g., write to S3, send email, etc.)
+    # ...
+
+    return {
+        'statusCode': 200,
+        'body': 'ENI check completed successfully.'
+    }
